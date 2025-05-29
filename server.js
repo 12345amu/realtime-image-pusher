705926cbd12eed602aa6e073c836ac4893700d49
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-29 10:45:56 +0530

    Set up input and output directories for image handling

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const WebSocket = require('ws');
+const fs = require('fs'); 
 
 const app = express();
 const PORT = 3000;
 
+const inputFolder = "C:\\Users\\HP\\OneDrive\\Pictures\\Screenshots";
+const outputFolder = path.join(__dirname, 'output');
+
+if (!fs.existsSync(outputFolder)) fs.mkdirSync(outputFolder);
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(outputFolder));
 
 const server = app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
